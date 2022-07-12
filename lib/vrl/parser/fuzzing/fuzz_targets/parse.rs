abe76e24d5315aaf997823f9e3a6fba929be5e31
sosipov <sosipov@rvision.ru>
2022-07-12 11:09:51 +0000

    afl-rs fuzzing

--- lib/vrl/parser/fuzzing/fuzz_targets/parse.rs
+++
@@ -1,6 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|s: String| {
-    let _ = vrl_parser::parse(&s);
-});
---
+++ lib/vrl/parser/fuzzing/src/parse.rs
@@ -0,0 +1,9 @@
+#[macro_use]
+extern crate afl;
+fn main() {
+ fuzz!(|data: &[u8]| {
+        if let Ok(s) = std::str::from_utf8(data) {
+            let _ =  vrl_parser::parse(&s);
+        }
+    });
+}
